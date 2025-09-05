3ee3095c4589ae39ce557dcd7643008741ce6f41
MarcelineVQ <marcythevq@gmail.com>
2025-09-05 16:43:51 -0700

    [ fix ] blizzard texture edge had stray pixels
    [ temp ] fix for twow icon path errors

--- make.sh
+++ make.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Build the MPQ file
 ./target/release/wow_dbc_patcher build --mpq patch-O.mpq
+
+# Check if --zip option is passed
+if [[ "$1" == "--zip" ]]; then
+    echo "Zipping patch-O.mpq to patch-O.zip..."
+    zip patch-O.zip patch-O.mpq
+    echo "Created patch-O.zip"
+fi
